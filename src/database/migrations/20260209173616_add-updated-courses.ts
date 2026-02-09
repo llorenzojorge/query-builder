fce969c51e957e3363010d6072dfb387eac781a2
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-02-09 14:48:11 -0300

    Criando migration para alterar tabela

---
+++ src/database/migrations/20260209173616_add-updated-courses.ts
@@ -0,0 +1,20 @@
+import { table } from "console";
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("courses", (table) => {
+    // TIMESTAMP = data + hora
+    // defaultTo define um valor automático, sendo o horário e data atual.
+    // AFTER cria a coluna updated_at depois da tabela de created_at
+    table.timestamp("updated_at").defaultTo(knex.fn.now()).after("created_at")
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("courses", (table) => {
+    table.dropColumn("updated_at")
+  })
+}
+
