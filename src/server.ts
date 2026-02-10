c90f86679c87dfbb3bf17c33f827c7ca78e6850c
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-02-10 16:54:20 -0300

    Metodo de Insert

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from "express"
+import { knex } from "./database/knex"
 
 const app = express()
 app.use(express.json())
 
 app.get("/", async (request: Request, response: Response) => {
   response.json({ message: "Hello World!" })
+})
+
+app.post("/courses", async (request: Request, response: Response) => {
+  const { name } = request.body
+
+  await knex("courses").insert({ name })
+
+  response.status(201).json()
 })
 
 app.listen(3333, () => console.log(`Server is running on port 3333`))
